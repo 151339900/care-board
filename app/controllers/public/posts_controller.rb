db0282bd4cdbdfb2bd0f361b57edbaa87fab7297
151339900 <165742289+151339900@users.noreply.github.com>
2024-07-16 10:34:30 +0000

    w

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -15,16 +15,23 @@ class Public::PostsController < ApplicationController
     post = Post.new(post_params)
     post.user_id = current_user.id
     post.save
-    redirect_to posts_path
+    redirect_to posts_path(post.id)
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
 
   private
