df506a462061139a1d02ec64305b19c3c78c6789
151339900 <165742289+151339900@users.noreply.github.com>
2024-07-17 10:20:41 +0000

    favorite

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ApplicationRecord
   has_one_attached :genre
   has_many :comments
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   belongs_to :user
+
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2024_07_16_100813) do
   end
 
   create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
